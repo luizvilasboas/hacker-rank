0e61b84e7032edcb3fc316bfbf3f0aa4c8516f33
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-03-19 15:52:06 -0300

    Add breaking best and worst records solution

---
+++ java/breaking-best-and-worst-records/Solution.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        int min = scores.get(0), max = scores.get(0), minCount = 0, maxCount = 0;
+
+        for (Integer score : scores) {
+            if (min > score) {
+                min = score;
+                minCount++;
+            }
+
+            if (max < score) {
+                max = score;
+                maxCount++;
+            }
+        }
+
+        return new ArrayList<Integer>(Arrays.asList(maxCount, minCount));
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        @SuppressWarnings("unused")
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> scores = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n");
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
