b27df83a3619b5de74980618487886f4f69740f8
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-03-20 12:47:24 -0300

    Add flipping bits solution

---
+++ java/flipping-bits/Solution.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.stream.*;
+
+class Result {
+    public static long flippingBits(long n) {
+        return (long) (Math.pow(2, 32) - (n + 1));
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                long n = Long.parseLong(bufferedReader.readLine().trim());
+
+                long result = Result.flippingBits(n);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
