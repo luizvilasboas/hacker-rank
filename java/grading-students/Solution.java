bdcd1bef632bb9b241078b9b0db049173c4af694
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-03-20 12:43:43 -0300

    Add grading students solution

---
+++ java/grading-students/Solution.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        for (int i = 0; i < grades.size(); i++) {
+            if (grades.get(i) < 38) {
+                continue;
+            }
+
+            int nextMutiple5Val = (int) (Math.ceil(grades.get(i) / 5.0) * 5);
+
+            if (nextMutiple5Val - grades.get(i) < 3) {
+                grades.set(i, nextMutiple5Val);
+            } else {
+                continue;
+            }
+        }
+
+        return grades;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int gradesCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> grades = IntStream.range(0, gradesCount).mapToObj(i -> {
+            try {
+                return bufferedReader.readLine().replaceAll("\\s+$", "");
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        })
+                .map(String::trim)
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.gradingStudents(grades);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining("\n"))
+                        + "\n");
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
