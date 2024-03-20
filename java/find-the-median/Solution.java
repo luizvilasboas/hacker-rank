42c8587ecb151ca23b50c90ab2527651ce7bc380
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-03-20 12:23:37 -0300

    Add find the median solution

---
+++ java/find-the-median/Solution.java
@@ -0,0 +1,35 @@
+import static java.util.stream.Collectors.toList;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+
+class Result {
+    public static int findMedian(List<Integer> arr) {
+        Collections.sort(arr);
+
+        return arr.get(arr.size() / 2);
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        @SuppressWarnings("unused")
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.findMedian(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
