5da96da4f67e80b73c1e8eedc2e93518f4515446
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-03-21 19:56:00 -0300

    Add pangrams solution

---
+++ java/pangrams/Solution.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static String pangrams(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for (char c : s.toLowerCase().toCharArray()) {
+            if (Character.isLetter(c)) {
+                map.put(c, 0);
+            }
+        }
+
+        return (map.size() == 26) ? "pangram" : "not pangram";
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String result = Result.pangrams(s);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
