d63cbb0e28994d69586ea173b5f5999fa250fbb1
Luiz Felipe de Castro Vilas Boas <luizfelipecastrovb@gmail.com>
2024-03-21 19:22:40 -0300

    Add counting sort 1 solution

---
+++ java/counting-sort-1/Solution.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static List<Integer> countingSort(List<Integer> arr) {
+        int[] ret = new int[100];
+
+        for (int num : arr) {
+            ret[num]++;
+        }
+
+        List<Integer> freq = new ArrayList<>();
+        for (int num : ret) {
+            freq.add(num);
+        }
+
+        return freq;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        @SuppressWarnings("unused")
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        List<Integer> result = Result.countingSort(arr);
+
+        bufferedWriter.write(
+                result.stream()
+                        .map(Object::toString)
+                        .collect(joining(" "))
+                        + "\n");
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
